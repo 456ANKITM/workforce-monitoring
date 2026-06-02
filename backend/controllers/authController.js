071c1abf37f8a099f0b36fcf6a80a2eb1d8be086
Ankit Mishra <am9491691@gmail.com>
2026-06-02 18:01:03 +0545

    changed cookie sameSite to none

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -41,7 +41,7 @@ export const signupAdmin = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true, // prevents JS access (XSS protection)
       secure: process.env.NODE_ENV === "production", // HTTPS only in prod
-      sameSite: "strict", // CSRF protection
+      sameSite: "none", // CSRF protection
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -172,7 +172,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true, // prevents JS access (XSS protection)
       secure: process.env.NODE_ENV === "production", // HTTPS only in prod
-      sameSite: "strict", // CSRF protection
+      sameSite: "none", // CSRF protection
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
